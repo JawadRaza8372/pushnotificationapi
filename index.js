642843676c5b5382005dd86a2c638b3dbba7b70c
Jawad Raza <jawadraza8372@gmail.com>
2022-04-01 09:26:06 +0500

    checking

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const admin = require("firebase-admin");
 const express = require("express");
 var cors = require("cors");
-const db = require("./firebase");
-const { collection, query, onSnapshot } = require("firebase/firestore");
 const myRoutes = require("./myRoutes");
 const app = express();
 
 // middleware
-app.use(express.json());
 
 app.use(cors());
+app.use(express.json());
+
 app.use(myRoutes);
 app.listen(5000, () => {
   console.log("SERVER IS UP AND RUNNING");
